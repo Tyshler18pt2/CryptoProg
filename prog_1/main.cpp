d0baf44ca1977988f8d4c9efa21a6e31670d2a2d
Tyshler18pt2 <119683149+Tyshler18pt2@users.noreply.github.com>
2023-03-29 01:53:46 +0300

    Update main.cpp

--- prog_1/main.cpp
+++ prog_1/main.cpp
@@ -1,29 +1,19 @@
-#include <string>
 #include <iostream>
-#include <string>
-#include <fstream>
-#include <cryptopp/cryptlib.h>
-#include <cryptopp/hex.h>
-#include <cryptopp/files.h>
-#include <cryptopp/md5.h> // MD5
-#include <cryptopp/pwdbased.h>
-#include <cryptopp/filters.h>
-using namespace std;
-int main()
+#include "hash.h"
+int main(int argc, char **argv)
 {
-	using CryptoPP::MD5;  // будем использовать расширение MD5 из библиотеки CRyproPP
-	ifstream fs;        //входной поток для чтения файлов
-	string str;
-	string strs = "";
-	string file = "../file.txt";  // в переменную файла указываем имя файла которое будем открывать для чтения
-	fs.open(file);  // открытие файла
-	while (!fs.eof()) {  //цикл,пока не достигнет конца файла
-		getline(fs, str); //считываем строку
-		strs = strs + str;  //в переменную strs добавляем эту строку
-	}
-	cout << strs;  // выводим на экран
-	string hesh;   // оперделяем строку для хранения хэша
-	CryptoPP::Weak1::MD5 hashmd4;  //определяем переменную для типа хэша из библиотеки CryptoPP
-	CryptoPP::StringSource(strs, true, new CryptoPP::HashFilter(hashmd4, new CryptoPP::HexEncoder(new CryptoPP::StringSink(hesh)))); //вычисление хэша от всех считанных строк с помощью библиотеки
-	cout << endl << hesh; //выводим на экран
-}
+    StringHash hash(Hash::SHA1);
+    std::string demo("hello, world");
+    std::cout << "Hash "<<hash.name() <<" from string \'"<<demo<<"\' is\n";
+    std::cout << hash(demo) << std::endl;
+
+    std::string psw("SuperPa$$w0rd");
+    StringHash hmac(psw, Hash::HMAC_SHA256);
+    std::string salt("qaMr4Gm3");
+    std::cout << "Hash "<<hmac.name() <<" from string \'"<<salt<<"\' on password \'"<<psw<<"\' is\n";
+    std::cout << hmac(salt) << std::endl;
+
+    FileHash fhash(Hash::SHA224);
+    std::string fname(argv[0]);
+    std::cout << "Hash "<<fhash.name() <<" from file \'"<<fname<<"\' is\n";
+    std::cout << fhash(fname) << std::endl;
